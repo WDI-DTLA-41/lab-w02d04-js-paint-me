63629200100e645e0200f8b2ad60092ff276bd91
Jake Miller <hillsighed@gmail.com>
2016-12-08 15:27:27 -0800

    Very basic functionality works. A lot of bugs to work out. still need to implement the clear button

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 console.log("hello.")
+
+var currentColor;
+// Select a color on the color picker
+// when i select a color put a border around it
+// document.querySelector('.color-selector').classList.add('selected-color')
+var selectColor = function (event){
+  console.log(event.target);
+  console.log(event);
+  //console.log(event);
+  //event.target.classList.add('select-color');
+  event.target.classList.toggle('select-color');
+  currentColor = event.target.classList[0];
+
+};
+
+// Add Event Listener to color selector div for the color palette
+$colSelectors = document.querySelector('.color-selectors')
+$colSelectors.addEventListener('click', selectColor);
+
+
+// Function for changing color
+var applyColor = function (event) {
+  console.log(event.target);
+  event.target.classList.toggle(currentColor);
+
+};
+
+// Add Event Listener on divTable for changing color
+$divTable = document.querySelector('.divTable');
+$divTable.addEventListener('click', applyColor);
