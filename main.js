1c2d02bdeadbc7c58ce6222eb1d8347a51c99045
brad morgan <bkmorgan3@gmail.com>
2016-12-08 15:57:38 -0800

    very basics

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('in');
+// console.log('hi');
+/*
+var table = document.querySelector('table');
+var tbody = document.querySelector('tbody');
+var tr = document.createElement('tr');
+var td = document.createElement('td');
+
+body.appendChild(table);
+table.appendChild(tbody);
+tbody.appendChild(tr);
+tr.appendChild(td);
+*/
