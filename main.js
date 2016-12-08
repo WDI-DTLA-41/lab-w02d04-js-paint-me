7c256d4cad997dd4ace10c144312dc65dc88c241
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-08 15:57:27 -0800

    idk

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 console.log("Finger painting with the insane");
 
+// set querySelectors
+var tbody = document.querySelector('tbody');
+
+
+// create a function to create Grid Table
+var createTable = function() {
+  var tr = document.createElement('tr');
+  var td1 = document.createElement('td');
+  var td2 = document.createElement('td');
+  var td3 = document.createElement('td');
+  var td4 = document.createElement('td');
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+  tr.appendChild(td4);
+  tbody.appendChild(tr);
+}
+
+createTable();
+
+
+
 
