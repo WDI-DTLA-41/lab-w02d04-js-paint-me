6cd085ad24db1a8c1be27d96463469b5ed2d8911
Baokhoa Vu <tealkrysta92@gmail.com>
2016-12-08 15:58:25 -0800

    paint stuff

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
 console.log('hello');
+
+var boxRed = document.getElementById('red-box');
+var boxBlue = document.getElementById('blue-box');
+var boxYellow = document.getElementById('yellow-box');
+
