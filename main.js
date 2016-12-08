5b1005bd7420c5f61abd677b5c5ae8bf1dd19059
Abi Munoz <abimunozr@gmail.com>
2016-12-08 15:25:46 -0800

    adds divs

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log("^^^^^^^^")
+
+var button = document.querySelector('.clear');
+var color1 = document.querySelector('#color1');
+var color2 = document.querySelector('#color2');
+var color3 = document.querySelector('#color3');
+
