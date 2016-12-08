73ee85e1a46d9c1cceb202f3f68788f660f7432b
dakotah walker <walker.dakotah@gmail.com>
2016-12-08 15:58:10 -0800

    adds grid and color pallet

---
+++ main.js
@@ -0,0 +1,20 @@
+// 1. create 3 colored boxes (red, blue, yellow).
+var redBox = document.querySelector('#red');
+var blueBox = document.querySelector('#blue');
+var yellowBox = document.querySelector('#yellow');
+
+// 2. create a game board grid of none-colored boxes.
+var gameBoard = document.querySelector('.gameBoard')
+
+// 3. when a user clicks on 1 of 3 colored boxes,
+//   the next box he clicks on will then be assigned
+//   that same color.
+//     -- when a box in the grid is paired with
+//     a colored box, its color property is then
+//     assigned to its match
+
+
+
+// 4. create a clear button
+// 5. when clicked, the clear button resets or 'clears'
+//   the color property of all grid boxes
