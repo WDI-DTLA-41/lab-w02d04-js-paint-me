564d02180d42bee579e6ee32c38f8582c4dea882
Barrett Quan <21barrett@gmail.com>
2016-12-08 19:16:55 -0800

    coloring grid fully functional

--- main.js
+++ main.js
@@ -10,14 +10,22 @@ yellow.value = 3;
 var color;
 var td = document.querySelector('td');
 var table = document.querySelector('table');
+var button = document.querySelector('button');
+var body = document.body;
 
 
 
 
 //function to save color value of what you clicked on
 var handleClick = function (event) {
+  if (event.target == button) console.log('you clicked button');
   console.log('clicked on grid', event.target)
-  event.target.style = "background: red";
+  if (event.target == table || event.target == button || event.target == body) return false;
+    else {
+      if (color == 1) event.target.style = "background: red";
+      else if (color == 2) event.target.style = "background: blue";
+      else if (color == 3) event.target.style = "background: yellow";
+    }
 }
 
 //function that saves what color you clicked on
@@ -34,6 +42,8 @@ var whatColor = function (event) {
   return color;
 }
 
+//clear function changes grid back to white
+
 //event listeners!
 paints.addEventListener('click', whatColor);
-table.addEventListener('click', handleClick);
+body.addEventListener('click', handleClick);
