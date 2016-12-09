d9aaf95c0869796fc49586af4ec0208aaaf84060
Ritwik Ruia <ritwikruia@gmail.com>
2016-12-09 09:13:58 -0800

    adds property select on color click, no paint yet

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 console.log('hello');
 
-var red = document.querySelector('#red');
+// var red = document.querySelector('.colorRed');
+var selectSquare = document.querySelector('.squares');
+var holdColor;
 
-var applyRed = function() {
+// var addRed = function(event) {
+//   event.target.classList.add('colorred')
+//   console.log('clicked');
+// };
 
-}
+// selectSquare.addEventListener('click', addRed);
+
+var selectRed = document.querySelector('#red');
+var selectBlue = document.querySelector('#blue');
+var selectYellow = document.querySelector('#yellow');
+var selectColors = document.querySelector('.colors');
+
+// var getColor = function(event) {
+//   if(event.target.)
+// }
+// var holdColor = function(){
+
+  selectColors.addEventListener('click', function(evt){
+    // console.log('click');
+    // console.log(evt.target);
+    // console.log(window.getComputedStyle( event.target , null).getPropertyValue('background-color'));
+    holdColor = window.getComputedStyle(event.target , null).getPropertyValue('background-color');
+    console.log(holdColor);
+});
+
+
+// }
