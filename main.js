28bc31efb98f867f2d3ccb533de933ac3fcd1b74
Tim Clark <oeomusic@gmail.com>
2016-12-08 23:31:30 -0800

    adds basic paint functionality

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-console.log("hello");
+
+
+// 3. function where if user clicks colored box, change colored box to
+// different class and style with larger border
+
+// 4. function where when user clicks box in grid, change background color
+// to color of selected box
+
+var color = "";
+
+// select all color seletors and store in variable
+var $selectors = document.querySelector('#color-selectors');
+
+// select each colored box and store in variable
+var $red = document.querySelector('red');
+var $blue = document.querySelector('blue');
+var $yellow = document.querySelector('yellow');
+
+// select grid container and store in variable
+var $grid = document.querySelector('#grid');
+
+
+var selectColor = function(event) {
+  if (event.target.classList.contains('color')) {
+    for (var i=0; i<event.currentTarget.children.length; i++) {
+    event.currentTarget.children[i].classList.remove('color-selected');
+    }
+    event.target.classList.toggle('color-selected');
+  }
+  color = event.target.id;
+}
+
+var changeColor = function(event) {
+  if (color === "red") {
+    event.target.style.backgroundColor = "red";
+  }  if (color === "blue") {
+    event.target.style.backgroundColor = "blue";
+  }  if (color === "yellow") {
+    event.target.style.backgroundColor = "yellow";
+  }
+  console.log(event.target.style.backgroundColor);
+}
+
+$selectors.addEventListener('click', selectColor);
+
+$grid.addEventListener('click', changeColor);
+
+$grid.children[0];
