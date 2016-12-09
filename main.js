430a1481ffb0a23a8669fa24f50c9b564ec9b634
dakotah walker <walker.dakotah@gmail.com>
2016-12-09 01:53:32 -0800

    adds color changing but no click event

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-// 1. create 3 colored boxes (red, blue, yellow).
+console.log('hello');
+
+// 1. create palette of 3 boxes with colored backgrounds (red, blue, yellow)
 var redBox = document.querySelector('#red');
 var blueBox = document.querySelector('#blue');
 var yellowBox = document.querySelector('#yellow');
+redBox.style.backgroundColor = "red";
+blueBox.style.backgroundColor = "blue";
+yellowBox.style.backgroundColor = "yellow";
 
-// 2. create a game board grid of none-colored boxes.
-var gameBoard = document.querySelector('.gameBoard')
+// 2. create a canvas of 12 boxes with white background
+var whiteBox = document.querySelector('#white');
+whiteBox.style.backgroundColor = "white";
+
+
+var colors = ["red", "blue", "yellow", "white"]; // delcare an array to contain four colors for each playable box
+var boxesInPlay = []; // declares an empty array that gets populated as boxes are picked
 
 // 3. when a user clicks on 1 of 3 colored boxes,
 //   the next box he clicks on will then be assigned
@@ -12,9 +22,39 @@ var gameBoard = document.querySelector('.gameBoard')
 //     -- when a box in the grid is paired with
 //     a colored box, its color property is then
 //     assigned to its match
+// var handleClick = function (evt) {
+//   var paintBrush ();
+// }
+
+// redBox.addEventListener('click', handleClick);
 
 
 
-// 4. create a clear button
-// 5. when clicked, the clear button resets or 'clears'
-//   the color property of all grid boxes
+
+// boxes
+
+whiteBox.addEventListener('click', changeColor); // when a blankBox is clicked, isTwoCards will run
+
+// changes box color
+var changeColor = function (color) {
+  document.querySelector('.box').style.background = color;
+}
+changeColor('green');
+
+
+
+// save later for assigning color to white boxes
+whiteBox.style.backgroundColor = "red";
+whiteBox.style.backgroundColor = "blue";
+whiteBox.style.backgroundColor = "yellow";
+
+
+// // 4. create a clear button
+// var btn = document.querySelector('button');
+// // 5. when clicked, the clear button resets or 'clears'
+// //   the color property of all grid boxes
+
+var isTwoBoxes = function() {
+  boxesInPlay.push(this.style.backgroundColor(box));
+    if (this.style.backgroundColor(white) != )
+}
