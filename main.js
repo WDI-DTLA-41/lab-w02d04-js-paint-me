79345b4e635875811ee75de755018f15a4e52324
Qian Chen <oda_san@hotmail.com>
2016-12-09 15:14:23 -0800

    MVP working on create

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ blue.addEventListener('click', handleClickColor);
 var btnClear = document.querySelector('.btnClear');
 btnClear.addEventListener('click', clear);
 var btnNewGrid = document.querySelector('.btnNewGrid');
-
+btnNewGrid.addEventListener('click', createGrid());
 
 var userColor;
-
-addListeners();
+var boxes = document.querySelectorAll('.box');
+addListeners(boxes);
 
 // function handles pick the color
 function handleClickColor(evt){
@@ -19,19 +19,16 @@ function handleClickColor(evt){
   evt.target.style.border = '5px solid black';
 }
 // function addsEventListener to all 'box'
-function addListeners(){
-  var boxes = document.querySelectorAll('.box');
-  for(var i=0; i<boxes.length; i++){
-    boxes[i].addEventListener('click', handleSetColor);
+function addListeners(arrayBoxes){
+  //var boxes = document.querySelectorAll('.box');
+  for(var i=0; i<arrayBoxes.length; i++){
+    arrayBoxes[i].addEventListener('click', handleSetColor);
   }
+  return arrayBoxes;
 }
 // function handles set color to box
 function handleSetColor(evt){
   evt.target.style.backgroundColor = userColor;
-}
-// function handles pick the square
-function handleClickSquare(evt){
-  evt.target.setAttribute('color', userColor);
 }
 // function clears the boxes
 function clear(evt){
@@ -40,3 +37,26 @@ function clear(evt){
     boxes[i].style.backgroundColor = '';
   }
 }
+function createGrid(){
+  var inputRows = document.createElement('input');
+  var inputBoxes = document.createElement('input');
+  var btnCreate = document.createElement('button');
+  inputRows.setAttribute('placeholder', 'Enter Height');
+  inputBoxes.setAttribute('placeholder', 'Enter Width');
+  btnCreate.textContent = 'Create';
+  document.querySelector('.buttons').appendChild(inputRows);
+  document.querySelector('.buttons').appendChild(inputBoxes);
+  document.querySelector('.buttons').appendChild(btnCreate);
+
+
+  btnCreate.addEventListener('click', getValues());
+
+}
+function getValues(){
+  var inputRowsValue = inputRows.value;
+  var inputBoxesValue = inputBoxes.value;
+}
+// function handles pick the square
+// function handleClickSquare(evt){
+//   evt.target.setAttribute('color', userColor);
+// }
