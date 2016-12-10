ad900e58a51ba32e0f0d6122e67434ff58f4470b
clear ls <ls>
2016-12-10 14:16:27 -0800

    was close on color boxes but not working

--- main.js
+++ main.js
@@ -22,43 +22,65 @@ addEventListener();
 
 // this is how you pick the color
 function selectColor(evt) {
-  // color is equal to lightblue on html
+  // color is equal to element inside 3 top boxes colored on html
+  // (lightblue, green, blue)
   pickedColor = evt.target.getAttribute('color');
+  // create 'if' to know which box is color chosen
   console.log(pickedColor);
-}
+
+  // reset pickedColor
+  var chgColor = document.querySelector('#startAgain')
+  chgColor = function(evt) {
+      console.log('change color');
+    }
+  }
+
 
 //lightBlueBox.addEventListener('click',selectColor);
 function addEventListener() {
-    //grab all boxes first
+    //grab all boxes first. class of .box is on grid boxes
     var boxes = document.querySelectorAll('.box');
     for(var i=0; i < boxes.length; i++) {
         // this [i] allows you to accesss what's in array
-        // addColor is the name of the function
+        // addColor is the name of the function to set up
+        // to change a color in the grid
         boxes[i].addEventListener('click',addColor);
         console.log(boxes[i]);
 
+    }
+  }
+
+
+// this is how to change color of square to new color
+var addColor = function(evt) {
+      // always true
+      console.log(this === evt.currentTarget);
+      // true when currentTarget and target are same object
+      console.log(this === evt.target);
+      // this.style.backgroundColor = 'blue';
+        if (evt.currentTarget !== 'lightBlueBox') {
+        console.log(this.style.backgroundColor = 'lightblue');
+
+      } else if (evt.currentTarget !== 'blueBox') {
+          this.style.backgroundColor = 'blue';
+      } else console.log(evt.currentTarget);
+
+}
+
+  // now, get a list of every element in the document
+var elements = document.getElementsByTagName('div');
+  // now, add a click listener so when element is clicked
+  // it turns blue
+    for(var i=0; i<elements.length; i++) {
+        elements[i].addEventListener('click',addColor, false);
 
     }
-}
-var addColor = function(evt) {
-
-  evt.target.// here=pickedColor;
-console.log('did i get here?');
-}
 
 
 
 
 
 
-
-
-// document.querySelector('div').
-
-// var chgColorToBlue function(evt) {
-//     clickedOn.style.backgroundColor = "blue";
-
-// }chgColorToBlue.addEventListener('click',changeColor,);
 
 
 
