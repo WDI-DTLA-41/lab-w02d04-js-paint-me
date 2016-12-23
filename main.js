a2f3feb852bff204b4c7b49f74d2455048401d64
clear ls <ls>
2016-12-23 10:26:43 -0800

    paint me finished

--- main.js
+++ main.js
@@ -1,86 +1,62 @@
-
-
 console.log("see me?");
 
-// var div = document.querySelector('div');
-var clearBox;
-var pickedColor;
-//target the div
-var lightBlueBox = document.querySelector('.lightBlueBox');
-var greenBox = document.querySelector('.greenBox');
-var blueBox = document.querySelector('.blueBox');
+// variables
+var colorOrange = document.querySelector('#orangeBox');
+var colorGreen = document.querySelector('#greenBox');
+var colorBlue = document.querySelector('#blueBox');
+var header = document.querySelector('header');
+var grid = document.querySelector('#grid');
+var container = document.querySelector('#container');
+var btnClear = document.querySelector('#clear');
 
-// this is for if i have a plain box and want to chg color
-//lightBlueBox.style.backgroundColor="lightBlueBox";
 
-// this creates the event listener to be able to click the box
-lightBlueBox.addEventListener('click',selectColor);
-greenBox.addEventListener('click',selectColor);
-blueBox.addEventListener('click',selectColor);
-console.log(pickedColor);
-addEventListener();
 
-// this is how you pick the color
-function selectColor(evt) {
-  // color is equal to element inside 3 top boxes colored on html
-  // (lightblue, green, blue)
-  pickedColor = evt.target.getAttribute('color');
-  // create 'if' to know which box is color chosen
-  console.log(pickedColor);
-
-  // reset pickedColor
-  var chgColor = document.querySelector('#startAgain')
-  chgColor = function(evt) {
-      console.log('change color');
+// create grid
+var createGrid = function() {
+  for (var col = 0; col < 3; col++) {
+    var tr = document.createElement('tr');
+    grid.appendChild(tr);
+    for (var row = 0; row < 4; row++) {
+      var td = document.createElement('td');
+      tr.appendChild(td);
     }
   }
-
-
-//lightBlueBox.addEventListener('click',selectColor);
-function addEventListener() {
-    //grab all boxes first. class of .box is on grid boxes
-    var boxes = document.querySelectorAll('.box');
-    for(var i=0; i < boxes.length; i++) {
-        // this [i] allows you to accesss what's in array
-        // addColor is the name of the function to set up
-        // to change a color in the grid
-        boxes[i].addEventListener('click',addColor);
-        console.log(boxes[i]);
-
-    }
-  }
-
-
-// this is how to change color of square to new color
-var addColor = function(evt) {
-      // always true
-      console.log(this === evt.currentTarget);
-      // true when currentTarget and target are same object
-      console.log(this === evt.target);
-      // this.style.backgroundColor = 'blue';
-        if (evt.currentTarget !== 'lightBlueBox') {
-        console.log(this.style.backgroundColor = 'lightblue');
-
-      } else if (evt.currentTarget !== 'blueBox') {
-          this.style.backgroundColor = 'blue';
-      } else console.log(evt.currentTarget);
-
 }
 
-  // now, get a list of every element in the document
-var elements = document.getElementsByTagName('div');
-  // now, add a click listener so when element is clicked
-  // it turns blue
-    for(var i=0; i<elements.length; i++) {
-        elements[i].addEventListener('click',addColor, false);
+// colored boxes
+var selectColor = function() {
+  if (event.target.id === 'orangeBox') {
+    grid.addEventListener('click',function() {
+      event.target.setAttribute('id','orangeBox');
+    });
+  }
+  if (event.target.id === 'greenBox') {
+    grid.addEventListener('click',function() {
+      event.target.setAttribute('id','greenBox');
+    });
+  }
+  if (event.target.id === 'blueBox') {
+    grid.addEventListener('click',function() {
+      event.target.setAttribute('id','blueBox');
+    });
+  }
+}
 
+var clearGrid = function() {
+  if(event.target.id === 'clear') {
+    for(var num = 0; num < tdList.length; num++) {
+      tdList[num].setAttribute('id','');
     }
+  }
+}
 
 
 
-
-
-
+// event listeners
+header.addEventListener('click',selectColor);
+btnClear.addEventListener('click',clearGrid);
+createGrid();
+var tdList = document.querySelectorAll('td');
 
 
 
